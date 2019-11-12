c50cfe9f3f25e3b01c5eb0bdf393f9a6f205690f
Michael Catterall <43209825+MyCatt@users.noreply.github.com>
2019-11-12 21:57:18 +1300

    Updates

---
+++ precache-manifest.667c91ba80c5928f9c51da488cf9b843.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60650282294c964443fb41ade2112421",
+    "url": "/Ticket-Quest/index.html"
+  },
+  {
+    "revision": "1a079f2808c700940006",
+    "url": "/Ticket-Quest/static/css/main.71420932.chunk.css"
+  },
+  {
+    "revision": "e53f1010954a11309a11",
+    "url": "/Ticket-Quest/static/js/2.c3d43cbe.chunk.js"
+  },
+  {
+    "revision": "1a079f2808c700940006",
+    "url": "/Ticket-Quest/static/js/main.02f3bd21.chunk.js"
+  },
+  {
+    "revision": "5304b347502b246aac7c",
+    "url": "/Ticket-Quest/static/js/runtime-main.e5a07eea.js"
+  },
+  {
+    "revision": "505863ed070d1d3a42208f7d4e99424a",
+    "url": "/Ticket-Quest/static/media/LogoTicket.505863ed.svg"
+  },
+  {
+    "revision": "47217994563c7aca9ef8d76cba7c97b4",
+    "url": "/Ticket-Quest/static/media/missing.47217994.svg"
+  }
+]);
